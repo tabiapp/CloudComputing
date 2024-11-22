fefdeefac71cf7ac86b54d3c390138da8f84577d
risqikhoirul <khoirulrizki029@gmail.com>
2024-11-22 13:35:06 +0000

    add cors

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const regionRoutes = require("./routes/regionRoutes");
 
 const app = express();
 
-// Middleware untuk parsing JSON
+// Middleware
 app.use(express.json());
+app.use(cors());
 
 // Route untuk API
 app.use("/api/regions", regionRoutes);
